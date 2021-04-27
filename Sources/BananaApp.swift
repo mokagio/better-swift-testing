bbdec1b331eaf4b0d892405b187862b433802d50
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-28 06:18:36 +1000

    Switch to UIKit lifecycle app retaining the SwifUI lifecycle capability

--- Sources/BananaApp.swift
+++ Sources/BananaApp.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
-@main
+// This is here just for reference if we wanted to switch to the pure SwiftUI life-cycle via the
+// Project.swift Tuist file.
+//
+// By removing the main annotation, the runtime won't load this.
+// @main
 struct BananaApp: App {
 
     var body: some Scene {
